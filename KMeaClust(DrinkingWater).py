f0f490ff01dc08ada09919c1c6d5a1a6a027bb10
Abuzar Bagewadi <55507269+abuzarbagewadi@users.noreply.github.com>
2020-05-31 14:47:28 +0530

    Add files via upload

---
+++ KMeaClust(DrinkingWater).py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 13:43:29 2020
+
+@author: Admin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 13:16:38 2020
+
+@author: Admin
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 00:12:59 2020
+
+@author: Admin
+"""
+
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('UpdatedDataset1.csv')
+X = dataset.iloc[:, [12, 13]].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 0:34])
+X[:, 0:34] = imputer.transform(X[:, 0:34])
+
+
+
+# Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Training the K-Means model on the dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'purple', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Medical & Education Budget')
+plt.xlabel('Drinking Water')
+plt.ylabel('Safe Drinking Water')
+plt.legend()
+plt.show()
