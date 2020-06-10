c294a48d54b6347f4261de41f9c59dbad77e077e
Abuzar Bagewadi <55507269+abuzarbagewadi@users.noreply.github.com>
2020-06-10 14:42:15 +0530

    Add files via upload

---
+++ H_Clust(SexRatio).py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 14:30:17 2020
+
+@author: Admin
+"""
+
+# Hierarchical Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('UpdatedDataset1.csv')
+X = dataset.iloc[:, [2, 3]].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 0:34])
+X[:, 0:34] = imputer.transform(X[:, 0:34])
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Sex Ratio')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Training the Hierarchical Clustering model on the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 3, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Heavily Depleting Sex Ratio')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Moderately Depleting Sex Ratio')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Lowly Depleting Sex Ratio')
+
+plt.title('Sex Ratio Study')
+plt.xlabel('Sex Ratio under 6 Years')
+plt.ylabel('Sex Ratio')
+plt.legend()
+plt.show()
